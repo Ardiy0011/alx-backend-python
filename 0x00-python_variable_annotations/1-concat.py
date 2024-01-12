7eaa4d664cdc176061ac3bfa16bf238e11230436
Ardiy0011 <ardiyquarsh@gmail.com>
2024-01-12 02:30:58 -0800

    type anotation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+""" function concat that takes a string str1
+and a string str2 as arguments """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Returns sum of two strings """
+    return str1 + str2
