22eccdbaf01c679ab2d03e100bee51f78cc58f96
Ardiy0011 <ardiyquarsh@gmail.com>
2024-01-15 18:04:05 -0800

    fix awating coroutine before printing

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" function that returns an async random delay """
+
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+async def measure_time(n: int = 0, max_delay: int = 0) -> float:
+    """ measure the total execution time for wait_n"""
+    start = time.perf_counter()
+    await wait_n(n, max_delay)
+    end = time.perf_counter()
+    return (end - start) / n
